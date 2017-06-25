9de54946dba9fd9fa8f8e391eb89256155d66e96
Liz M <lizmacdonald45@gmail.com>
2017-06-25 13:47:51 -0600

    adding problem 5 alternate code

--- 5_Computing_GC_content/problem_5.py
+++ 5_Computing_GC_content/problem_5.py
@@ -8,7 +8,8 @@ spdat = data.split(">Rosalind_")
 
 
 
-for i in spdat:
+for set in spdat:
+	set.find("GC")
 	gc_count = 0
 	len_count = len(i) - 4
 	for t in i:
@@ -23,3 +24,23 @@ for i in spdat:
 	print (str("Rosalind_" + str(i[0:4])))
 	print (str('GC: ' + str(gc_percent)))
 
+
+path = '/Users/elizabethmacdonald/desktop/python/Rosalind-problems/5_Computing_GC_content/rosalind_gc.txt'
+with open(path, 'r') as prob1:
+    data = prob1.read()
+
+spdat = []
+spdat = data.split(">Rosalind_")
+
+for fastaRaw in spdat[1::]:
+	index = fastaRaw.find('\n') + 1
+	fasta = fastaRaw[index::]
+	fasta = fasta.replace('\n', '')
+	length = len(fasta)
+	gc_count = 0
+	for seq in fasta:
+		if seq == "C" or seq == "G":
+			gc_count = gc_count + 1
+	gc_percent = gc_count/length * 100
+	print (str("Rosalind_" + str(fastaRaw[0:4])))
+	print (str('GC: ' + str(gc_percent)))
